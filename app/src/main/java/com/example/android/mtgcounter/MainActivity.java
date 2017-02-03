b495025b8ff3a16e706070cfb70aebe290d9d5ea
Paris Galanis <galanisparis@hotmail.com>
2017-02-03 11:49:41 +0200

    joining functions that are similar

--- app/src/main/java/com/example/android/mtgcounter/MainActivity.java
+++ app/src/main/java/com/example/android/mtgcounter/MainActivity.java
@@ -10,111 +10,116 @@ public class MainActivity extends AppCompatActivity {
     int Player2Poison = 0;
     int Player1Life = 20;
     int Player2Life = 20;
+    TextView PoisonView1;
+    TextView PoisonView2;
+    TextView LifeView1;
+    TextView LifeView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayPlayer1Life(Player1Life);
-        displayPlayer2Life(Player1Life);
-        displayPlayer1Poison(Player1Poison);
-        displayPlayer2Poison(Player2Poison);
+        PoisonView1 = (TextView) findViewById(R.id.Player1Poison);
+        PoisonView2 = (TextView) findViewById(R.id.Player2Poison);
+        LifeView1 = (TextView) findViewById(R.id.Player1Life);
+        LifeView2 = (TextView) findViewById(R.id.Player2Life);
+        displayLife(LifeView1, Player1Life);
+        displayLife(LifeView2, Player2Life);
+        displayPoison(PoisonView1, Player1Poison);
+        displayPoison(PoisonView2, Player2Poison);
+        displayLife(LifeView1, Player1Life);
+        displayLife(LifeView2, Player2Life);
     }
+
     /**
      * Add life to player 1
      */
-    public void addLifeToPlayer1 (View v) {
+    public void addLifeToPlayer1(View v) {
         Player1Life = Player1Life + 1;
-        displayPlayer1Life(Player1Life);
+        displayLife(LifeView1, Player1Life);
     }
+
     /**
      * Remove life from player 1
      */
-    public void removeLifeFromPlayer1 (View v) {
+    public void removeLifeFromPlayer1(View v) {
         Player1Life = Player1Life - 1;
-        displayPlayer1Life(Player1Life);
+        displayLife(LifeView1, Player1Life);
     }
+
     /**
      * Add life to player 2
      */
-    public void addLifeToPlayer2 (View v) {
+    public void addLifeToPlayer2(View v) {
         Player2Life = Player2Life + 1;
-        displayPlayer2Life(Player2Life);
+        displayLife(LifeView2, Player2Life);
     }
+
     /**
      * Remove life from player 2
      */
-    public void removeLifeFromPlayer2 (View v) {
+    public void removeLifeFromPlayer2(View v) {
         Player2Life = Player2Life - 1;
-        displayPlayer2Life(Player2Life);
+        displayLife(LifeView2, Player2Life);
     }
+
     /**
      * Add poison to player 1
      */
-    public void addPoisonToPlayer1 (View v) {
+    public void addPoisonToPlayer1(View v) {
         Player1Poison = Player1Poison + 1;
-        displayPlayer1Poison(Player1Poison);
+        displayPoison(PoisonView1, Player1Poison);
     }
+
     /**
      * Remove poison from player 1
      */
-    public void removePoisonFromPlayer1 (View v) {
+    public void removePoisonFromPlayer1(View v) {
         Player1Poison = Player1Poison - 1;
-        displayPlayer1Poison(Player1Poison);
+        displayPoison(PoisonView1, Player1Poison);
     }
+
     /**
      * Add poison to player 2
      */
-    public void addPoisonToPlayer2 (View v) {
+    public void addPoisonToPlayer2(View v) {
         Player2Poison = Player2Poison + 1;
-        displayPlayer2Poison(Player2Poison);
+        displayPoison(PoisonView2, Player2Poison);
     }
+
     /**
      * Remove poison from player 2
      */
-    public void removePoisonFromPlayer2 (View v) {
+    public void removePoisonFromPlayer2(View v) {
         Player2Poison = Player2Poison - 1;
-        displayPlayer2Poison(Player2Poison);
+        displayPoison(PoisonView2, Player2Poison);
     }
+
     /**
      * Reset life and poison
      */
-    public void resetLife (View v) {
+    public void resetLife(View v) {
         Player1Life = 20;
         Player2Life = 20;
         Player1Poison = 0;
         Player2Poison = 0;
-        displayPlayer1Life(Player1Life);
-        displayPlayer2Life(Player2Life);
-        displayPlayer1Poison(Player1Poison);
-        displayPlayer2Poison(Player2Poison);
+        displayLife(LifeView2, Player2Life);
+        displayLife(LifeView2, Player2Life);
+        displayPoison(PoisonView1, Player1Poison);
+        displayPoison(PoisonView2, Player2Poison);
     }
+
     /**
-     * Displays the given life of player 1
+     * Displays the life
      */
-    public void displayPlayer1Life(int life) {
-        TextView lifeView = (TextView) findViewById(R.id.Player1Life);
+    public void displayLife(TextView lifeView, int life) {
         lifeView.setText(String.valueOf(life));
     }
+
     /**
-     * Displays the given poison of player 1
+     * Displays the poison
      */
-    public void displayPlayer1Poison(int poison) {
-        TextView PoisonView = (TextView) findViewById(R.id.Player1Poison);
-        PoisonView.setText(String.valueOf(poison));
-    }
-    /**
-     * Displays the given life of player 2
-     */
-    public void displayPlayer2Life(int life) {
-        TextView lifeView = (TextView) findViewById(R.id.Player2Life);
-        lifeView.setText(String.valueOf(life));
-    }
-    /**
-     * Displays the given poison of player 2
-     */
-    public void displayPlayer2Poison(int poison) {
-        TextView PoisonView = (TextView) findViewById(R.id.Player2Poison);
-        PoisonView.setText(String.valueOf(poison));
+    public void displayPoison(TextView poisonView, int poison) {
+        poisonView.setText(String.valueOf(poison));
     }
 }
